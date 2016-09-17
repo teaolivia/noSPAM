1e332263443581eebf9cda4df854a5dfcba64595
squilliams <bimoaryo@rocketmail.com>
2016-09-17 15:43:10 +0700

    Attempt to OpenCSV

--- noSPAM/src/nospam/NoSPAM.java
+++ noSPAM/src/nospam/NoSPAM.java
@@ -4,7 +4,14 @@
  * and open the template in the editor.
  */
 package nospam;
+
 import IndonesianNLP.*;
+import com.opencsv.*;
+import java.io.*;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author theaolivia
@@ -15,7 +22,24 @@ public class NoSPAM {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            // convert CSV to List
+            CSVReader reader;
+            reader = new CSVReader(new FileReader("dataset.csv"));
+            List myEntries = reader.readAll();
+            
+//            for (int i=0; i < myEntries.size(); i++){
+//                System.out.println(myEntries.get(i));                
+//            }
+            System.out.println("SSSSS");
+            String [] nextLine;
+            while ((nextLine = reader.readNext()) != null) {
+               // nextLine[] is an array of values from the line
+               System.out.println(nextLine[0]);
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(NoSPAM.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
     }
-    
 }
