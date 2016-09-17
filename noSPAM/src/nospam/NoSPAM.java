619dd6b51ee741c0db8ec89b4bc6c0702932635d
squilliams <bimoaryo@rocketmail.com>
2016-09-17 17:39:50 +0700

    cobain inanlp dong

--- noSPAM/src/nospam/NoSPAM.java
+++ noSPAM/src/nospam/NoSPAM.java
@@ -12,6 +12,7 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.Arrays;
+import java.util.ArrayList;
 
 /**
  *
@@ -27,19 +28,22 @@ public class NoSPAM {
             // convert CSV to List
             CSVReader reader;
             reader = new CSVReader(new FileReader("d:/dataset.csv"));
-//            List myEntries = reader.readAll();
-            
-//            for (int i=0; i < myEntries.size(); i++){
-//                String represent = myEntries.get(i).toString();
-//                System.out.println(represent);                
-//            }
-            String [] nextLine;
-            while ((nextLine = reader.readNext()) != null) {
-               // nextLine[] is an array of values from the line
-               if (nextLine!=null){
-                    System.out.println(Arrays.toString(nextLine));
-               }
+            List<String[]> myEntries = reader.readAll();
+           
+            for(String[] row : myEntries){
+                System.out.println(Arrays.toString(row));
             }
+            
+            IndonesianSentenceTokenizer tokenizer = new IndonesianSentenceTokenizer();
+            ArrayList<String> token = tokenizer.tokenizeSentence("Coba masukin kalimat yang perlu di tokenizer disini");
+
+//        String [] nextLine;
+//            while ((nextLine = reader.readNext()) != null) {
+//               // nextLine[] is an array of values from the line
+//               if (nextLine!=null){
+//                    System.out.println(Arrays.toString(nextLine));
+//               }
+//            }
         } catch (IOException ex) {
             Logger.getLogger(NoSPAM.class.getName()).log(Level.SEVERE, null, ex);
         }
